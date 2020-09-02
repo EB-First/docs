12c56e2cfe3d9752318bcc6cdee2c0798698e162
vmehrotra50 <varun.mehrotra50@gmail.com>
2020-09-02 12:31:49 -0400

    Change interface

--- conf.py
+++ conf.py
@@ -1,8 +1,9 @@
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# http://www.sphinx-doc.org/en/master/config
 
 # -- Path setup --------------------------------------------------------------
 
@@ -15,30 +16,42 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 
-import sphinx_rtd_theme
-
-
 # -- Project information -----------------------------------------------------
 
 project = 'EB FIRST'
-copyright = '2020, East Brunswick FIRST Inc.'
+copyright = '2020, EB FIRST Inc.'
 author = 'Varun Mehrotra'
-master_doc = 'index'
+version = '2020'
+
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark', 'sphinx_rtd_theme']
+extensions = [
+    'sphinx_tabs.tabs',
+    'sphinx.ext.imgmath',
+    'sphinx.ext.todo',
+    'sphinx.ext.graphviz',
+    'sphinx.ext.autosectionlabel',
+    'sphinxcontrib.ghcontributors',
+    'sphinxcontrib.remoteliteralinclude',
+    'notfound.extension'
+]
+
+# TODO Directives omit a warning
+todo_emit_warnings = False
+
+# TODO Directives are not shown in output
+todo_include_todos = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# Specify the master doc file, AKA our homepage
+master_doc = "index"
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -46,25 +59,58 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-
-html_theme_options = {
-    'canonical_url': '',
-    'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+	'collapse_navigation': True,
+	'sticky_navigation': False,
+	'titles_only': True
+}
+
+user_options = [
+        ('warning-is-error', True),
+]
+
+
+# -- Options for latex generation --------------------------------------------
+
+latex_engine = 'xelatex'
+
+
+
+latex_elements = {
+    'fontpkg': r'''
+	\setmainfont{DejaVu Serif}
+	\setsansfont{DejaVu Sans}
+	\setmonofont{DejaVu Sans Mono}''',
+    'preamble': r'''
+	\usepackage[titles]{tocloft}
+	\cftsetpnumwidth {1.25cm}\cftsetrmarg{1.5cm}
+	\setlength{\cftchapnumwidth}{0.75cm}
+	\setlength{\cftsecindent}{\cftchapnumwidth}
+	\setlength{\cftsecnumwidth}{1.25cm}
+	''',
+    'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
+    'printindex': r'\footnotesize\raggedright\printindex',
+}
+
+suppress_warnings = ['epub.unknown_project_files']
+
+sphinx_tabs_valid_builders = ['epub', 'linkcheck']
+© 2020 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
